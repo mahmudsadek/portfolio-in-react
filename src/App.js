f90214775a11d7efdfd58e0cb1812023192c7f6d
sadek <mahmoudsadek.1905@gmail.com>
2024-04-20 10:49:29 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./componants/Hero/hero";
+import Bio from "./componants/Bio/Bio";
+import Skills from "./componants/Skills/Skills";
+import Eductaion from "./componants/Education/Education";
+import Footer from "./componants/Footer/footer";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+// import your icons
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Hero />
+      <Bio />
+      <Skills />
+      <Eductaion />
+      <Footer />
+		</div>
+	);
 }
 
 export default App;
